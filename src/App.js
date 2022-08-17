0f508d4e5a6074e9699607a753f3dd3a55a5174c
Angkana Boonya <mayangkanaboonya@gmail.com>
2022-08-17 15:08:47 +0700

    Components & Props (4)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import './App.css';
+import WordCard from "./WordCard";
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello {"World"}
+       <WordCard value="hello"/>
     </div>
   );
 }
