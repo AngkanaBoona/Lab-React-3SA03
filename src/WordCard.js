3c2598286281fa99fc31d65e6d2ff6a96a1a0551
Angkana Boonya <mayangkanaboonya@gmail.com>
2022-08-19 23:27:12 +0700

    Add Features (3)

--- src/WordCard.js
+++ src/WordCard.js
@@ -27,9 +27,12 @@ export default function WordCard(props){
        if(guess.length == state.word.length){
          if(guess == state.word){
          console.log('yeah!')
+         console.log('congratulations')
          setState({...state, guess: '', completed: true})
          }else{
          console.log('reset')
+         console.log('Try again..')
+         console.log('Fighting')
          setState({...state, guess: '', attempt: state.attempt + 1})
          }
          }
