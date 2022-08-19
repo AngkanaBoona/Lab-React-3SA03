2a67c7e7ca985c4030a0e987c93173eb74d1fcf3
Angkana Boonya <mayangkanaboonya@gmail.com>
2022-08-19 23:36:57 +0700

    Add Header (4)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ const word = "ComputerEngineering";
 function App() {
   return (
     <div>
+      <div class="value">
+      <header>
+      <h1>Word Game</h1>
+      </header>
+      <p>All About Coe</p>
+
+      <button type="button"
+      onclick="document.getElementById('test').innerHTML = Date()">
+        Spell the words correctly.
+
+        </button>
+          </div>
        <WordCard value="Hello"/>
        <WordCard value="Hardware"/>
        <WordCard value="Software"/>
