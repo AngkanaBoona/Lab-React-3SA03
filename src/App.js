534c7afd25384ee83641408cec0d53cc2e89e928
Angkana Boonya <mayangkanaboonya@gmail.com>
2022-08-19 23:02:40 +0700

    Add Word (1)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import './App.css';
 import WordCard from "./WordCard";
 
-const word = "Hello";
+const word = "ComputerEngineering";
 function App() {
   return (
     <div>
-       <WordCard value="hello"/>
+       <WordCard value="Hello"/>
+       <WordCard value="Hardware"/>
+       <WordCard value="Software"/>
+       <WordCard value="Computer"/>
     </div>
   );
 }
